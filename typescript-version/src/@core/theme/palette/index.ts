c5c91eff3ab88c4e3174a823260f61f1e3a50168
teddy cubaka <teddycubaka96@gmail.com>
2024-02-23 13:53:19 +0100

    style : change colors

--- typescript-version/src/@core/theme/palette/index.ts
+++ typescript-version/src/@core/theme/palette/index.ts
@@ -10,7 +10,7 @@ const DefaultPalette = (mode: PaletteMode, themeColor: ThemeColor) => {
 
   const primaryGradient = () => {
     if (themeColor === 'primary') {
-      return '#C6A7FE'
+      return '#43b843'
     } else if (themeColor === 'secondary') {
       return '#9C9FA4'
     } else if (themeColor === 'success') {
@@ -36,9 +36,9 @@ const DefaultPalette = (mode: PaletteMode, themeColor: ThemeColor) => {
     },
     mode: mode,
     primary: {
-      light: '#9E69FD',
-      main: '#9155FD',
-      dark: '#804BDF',
+      light: '#28f028',
+      main: '#43b843',
+      dark: '#268c26',
       contrastText: '#FFF'
     },
     secondary: {
